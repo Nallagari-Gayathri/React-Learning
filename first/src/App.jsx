ba2d405a78129a3ff9c8ad7bdd6e6a9b4304d936
Nallagari-Gayathri <nallagarigayathri2004@gmail.com>
2023-12-09 13:04:10 -0500

    My boiler code

---
+++ first/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import New from './components/New'
+import Button from './components/Button/Button'
+
+
+function App() {
+  return(
+    <>
+      <div>Home Page</div>
+    </>
+  )
+}
+
+export default App
